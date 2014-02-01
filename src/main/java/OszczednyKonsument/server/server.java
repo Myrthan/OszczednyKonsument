ae14a4690b3a47458f17f437a90b00704f7184d1
Myrthan <glofynder@gmail.com>
2014-02-01 15:54:14 +0100

    Początek

---
+++ src/main/java/OszczednyKonsument/server/server.java
@@ -0,0 +1,13 @@
+package OszczednyKonsument.server;
+
+public class server {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
