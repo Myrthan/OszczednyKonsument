27776d406acf6f0e8aa8307b5fc367e25c1c5e0a
Myrthan <glofynder@gmail.com>
2014-02-01 16:02:25 +0100

    Poprawka

--- src/main/java/OszczednyKonsument/server/Server.java
+++ src/main/java/OszczednyKonsument/server/Server.java
@@ -1,13 +1,13 @@
 package OszczednyKonsument.server;
 
-public class server {
+public class Server {
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("test");
 	}
 
 }
